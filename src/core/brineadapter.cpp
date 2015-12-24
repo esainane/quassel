e01d26608f40735470361a9f94b4a8b14ac95acf
esainane <esainane+github@gmail.com>
2015-12-24 13:05:15 +1300

    Run brine event loop

--- src/core/brineadapter.cpp
+++ src/core/brineadapter.cpp
@@ -5,12 +5,35 @@
 
 #include "brineadapter.h"
 
+#include <brine.h>
+
 BrineAdapter *brineAdapter;
 
 BrineAdapter::BrineAdapter()
 {
 }
 
+
+int BrineAdapter::net_msgrecv(const void *connection, const char *message, int type)
+{
+    return 0;
+}
+int BrineAdapter::user_msgrecv(const void *connection, const char *message, const char *user)
+{
+    return 0;
+}
+int BrineAdapter::chan_msgrecv(const void *connection, const char *message, const char *channel)
+{
+    return user_msgrecv(connection, message, channel); // FIXME
+}
+
 void BrineAdapter::run()
 {
+    struct brine b = {
+        .net_msgrecv = net_msgrecv,
+        .user_msgrecv = user_msgrecv,
+        .chan_msgrecv = chan_msgrecv
+    };
+    brine_init(&b);
+    brine_eventloop();
 }
--- src/core/brineadapter.h
+++ src/core/brineadapter.h
@@ -2,12 +2,21 @@
 #define BRINEADAPTER_H
 
 #include <QThread>
+#include <QHash>
+
+#include "corenetwork.h"
+
 class BrineAdapter : public QThread
 {
 public:
     BrineAdapter();
 
     void run();
+private:
+    QHash<const void *, CoreNetwork *> _connections;
+    static int net_msgrecv(const void *connection, const char *message, int type);
+    static int user_msgrecv(const void *connection, const char *message, const char *user);
+    static int chan_msgrecv(const void *connection, const char *message, const char *channel);
 };
 
 extern BrineAdapter *brineAdapter;
